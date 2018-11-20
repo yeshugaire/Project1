38d3e7d56d761591552d0261f69bd4071f927124
tylerpilette <tyler.pilette@gmail.com>
2018-11-19 20:22:07 -0700

    fixed redirect

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,3 @@
 $("button").on("click", function(){
-    window.location.href="https://accounts.spotify.com/en/authorize?response_type=token&client_id=15520dd0394a472e86bd02e5f89c43e1&redirect_uri=https:%2F%2Fyeshugaire.github.io%2FProject1%2F"
+    window.location.href="https://accounts.spotify.com/en/authorize?response_type=token&client_id=15520dd0394a472e86bd02e5f89c43e1&redirect_uri=https:%2F%2Fyeshugaire.github.io%2FProject1%2Fmain.html"
 })
