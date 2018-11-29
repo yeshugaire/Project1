09cedd7ecb9b6a1ac98ba8cd83994c93cf12d9ae
Daniel L <43019017+funkedee@users.noreply.github.com>
2018-11-28 20:47:48 -0700

    displays playlist tracks and features- breaks on long playlists

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,40 +4,192 @@ var parsedHash = hash.split("&");
 accessToken = parsedHash[0];
 console.log(accessToken);
 
-
 // make ajax call to get user playlist
 $.ajax({
     url: "https://api.spotify.com/v1/me/playlists",
     headers: {
         "Authorization": "Bearer " + accessToken
     },
-    success:  function(response) {
-        console.log(response);
-
-
-        
+    success: function (response) {
         // display playlists in table
-        for (var i=0; i<response.items.length; i++) {
-            var data= response.items[i]
-            $("#user-playlist-table").append("<tr id='playlist' data-playlist-id= '"+ data.id + "'><td>"+ data.name +"</td><td>"+ data.tracks.total + "</td><td>"+ data.owner.display_name +"</td></tr>");
+        for (var i = 0; i < response.items.length; i++) {
+            var data = response.items[i]
+            $("#user-playlist-table").append("<tr class='playlist' data-playlist-id= '" + data.id + "' data-total-tracks= '" + data.tracks.total + "'><td>" + data.name + "</td><td>" + data.tracks.total + "</td><td>" + data.owner.display_name + "</td></tr>");
             console.log(data.name);
         };
-        
+
     },
 });
 
-// //when you choose a playlist 
-// $("#playlist").on("click", function() {
-//     var currentPlaylistID= $(this).attr("data-playlist-id");
+//when you choose a playlist 
+$(document).on("click", ".playlist", function () {
+    // get playlist id
+    var currentPlaylistID = $(this).attr("data-playlist-id");
 
-//     // make ajax call for playlists tracks
-//     $.ajax({
-//         url: "https://api.spotify.com/v1/playlists/" + currentPlaylistID + "/tracks",
-//         headers: {
-//             "Authorization": "Bearer " + accessToken
-//         },
-//         success: function(response) {
-//             console.log(response);
-//         },
-//     });
-// });
+    // make table for current playlist
+    var currentPlaylistTableDiv = $("<div>");
+    var currentPlaylistTable = $("<table id='table_id' class='display'><thead><tr><th>Name</th><th>Artist</th><th>Album</th><th>Length</th><th>Tempo(BPM)</th><th>Key</th><th>Mode</th><th>Energy</th><th>Danceability</th><th>Euphoria</th><th>Time Signature</th></tr></thead><tbody id= 'current-playlist-table'</tbody></table>");
+
+    // clear playlist table if choosing new playlist
+    currentPlaylistTableDiv.empty();
+
+    // variables and loop for if playlist is >100 tracks
+    var offset = 0;
+    var loopEnd = $(this).attr("data-total-tracks");
+    for (i = 0; i < loopEnd/100; i++) {
+        //  ajax call for tracks
+        $.ajax({
+            url: "https://api.spotify.com/v1/playlists/" + currentPlaylistID + "/tracks?offset=" + offset,
+            headers: {
+                "Authorization": "Bearer " + accessToken
+            },
+            success: function (response) {
+                var trackIds = "";
+                console.log(response);
+                // loop through all tracks in response
+                for (i = 0; i < response.items.length; i++) {
+                    // add track id to trackIds string
+                    var data = response.items[i].track;
+                    trackIds = trackIds + data.id + ",";
+
+                    // variables for table
+                    var trackArtists = "";
+
+                    // // loop through track artists for mulutiple artists
+                    // if (data.artists.length > 1) {
+                    //     for (i = 0; i < data.artists.length; i++) {
+                    //         trackArtists = trackArtists + data.artists[i].name + ", ";
+                    //     };
+                    // };
+                    // else {
+                            trackArtists= data.artists[0].name;
+                    // };
+
+                    // make table row and add name, ablum and artist to it
+                    var tableRow = $("<tr data-track-id='"+ data.id +"'><td>" + data.name + "</td><td>" + trackArtists + "</td><td>" + data.album.name + "</td></tr>");
+                    $("#current-playlist-table").append(tableRow);
+                    
+                };
+                console.log(trackIds);
+                // make ajax call for track features
+                $.ajax({
+                    url: "https://api.spotify.com/v1/audio-features?ids=" + trackIds,
+                    headers: {
+                        "Authorization": "Bearer " + accessToken
+                    },
+                    success: function (response) {
+                        console.log(response);
+                        var info= response.audio_features;
+                        console.log(info)
+
+                        // loop through tracks and display track features
+                        for (f=0; f<info.length; f++){
+                            console.log(info[f].id)
+                            var info= response.audio_features;
+                            
+                            for (j=0; j<loopEnd; j++ )
+                                if ($($("#current-playlist-table tr")[j]).attr("data-track-id") === info[f].id) {
+                                    // variables for track features
+                                    var key
+                                    var mode
+                                    var timeSignature
+
+                                    // if statements for key
+                                if (info[f].key === 0){
+                                        key= "C";
+                                    };
+                                if (info[f].key === 1){
+                                        key= "C#/Db";
+                                    };
+                                if (info[f].key === 2){
+                                        key= "D";                           
+                                    };
+                                if (info[f].key === 3){
+                                        key= "D#/Eb";
+                                    };
+                                if (info[f].key === 4){
+                                        key= "E";
+                                    };
+                                if (info[f].key === 5){
+                                        key= "F";
+                                    };
+                                if (info[f].key === 6){
+                                        key= "F#/Gb";
+                                    };
+                                if (info[f].key === 7){
+                                        key= "G";
+                                    };
+                                if (info[f].key === 8){
+                                        key= "G#/Ab";
+                                    };
+                                if(info[f].key === 9){
+                                        key= "A";
+                                    };
+                                if (info[f].key === 10){
+                                        key= "A#/Bb";
+                                    };
+                                if (info[f].key === 11){
+                                        key= "B";
+                                    };
+                                if (info[f].key === 12){
+                                        key= "C";
+                                    };
+
+                                    // if statements for mode
+                                if (info[f].mode === 0) {
+                                        mode = "Minor";
+                                    };
+
+                                if (info[f].mode === 1) {
+                                        mode = "Major"
+                                    };
+
+                                    // if statements for time signature
+                                if (info[f].time_signature === 3) {
+                                        timeSignature = "3/4";
+                                    };
+                                if (info[f].time_signature === 4) {
+                                        timeSignature = "4/4";
+                                    };
+                                if (info[f].time_signature === 5) {
+                                        timeSignature = "5/4";
+                                    };
+                                if (info[f].time_signature === 6) {
+                                        timeSignature = "6/4";
+                                    };
+                                if (info[f].time_signature === 7) {
+                                        timeSignature = "7/4";
+                                    };
+
+                                    // calulate duration
+                                    function millisToMinutesAndSeconds(millis) {
+                                        var minutes = Math.floor(millis / 60000);
+                                        var seconds = ((millis % 60000) / 1000).toFixed(0);
+                                        return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+                                    };
+
+                                    // append features to table
+                                    var appendToList= function (property) {
+                                    $($("#current-playlist-table tr")[f]).append(property);
+                                    };
+
+                                    appendToList("<td>"+ millisToMinutesAndSeconds(info[f].duration_ms) + "</td>");
+                                    appendToList("<td>"+ info[f].tempo +"</td>");
+                                    appendToList("<td>"+ key+"</td>");
+                                    appendToList("<td>"+ mode +"</td>");
+                                    appendToList("<td>"+ info[f].energy +"</td>");
+                                    appendToList("<td>"+ info[f].danceability +"</td>");
+                                    appendToList("<td>"+ info[f].valence +"</td>");
+                                    appendToList("<td>"+ timeSignature+"</td>");
+                                    
+                                };
+                        };
+                    },
+                });
+            },
+        });
+        offset = offset + 100;
+    };
+    currentPlaylistTableDiv.append(currentPlaylistTable);
+    $(".container").append(currentPlaylistTableDiv);
+});
